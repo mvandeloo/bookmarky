6e7fc2ab0d3b011fca10a83a862e9aba2262cf24
mvandeloo <martin.vandeloo@googlemail.com>
2013-09-26 13:03:10 +0100

    data check

--- lib/user.rb
+++ lib/user.rb
@@ -28,12 +28,15 @@ attr_accessor :password_confirmation
   property :password_digest, Text
   validates_confirmation_of :password
 
+
   # when assigned the password, we don't store it directly
   # instead, we generate a password digest, that looks like this:
   # "$2a$10$vI8aWBnW3fID.ZQ4/zo1G.q1lRps.9cGLcZEiGDMVr5yUP1KUOYTa"
   # and save it in the database. This digest, provided by bcrypt,
   # has both the password hash and the salt. We save it to the 
   # database instead of the plain password for security reasons.
+  validates_uniqueness_of :email
+  property :email, String, :unique => true
 
   def password=(password)
     @password = password
